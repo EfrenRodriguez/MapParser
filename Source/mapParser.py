17c8cb060bb3b87565f9f70350888fe74cde73a5
240024053 <joseefren.rodriguez@geappliances.com>
2020-05-15 16:41:47 -0400

    With Folder name

--- Source/mapParser.py
+++ Source/mapParser.py
@@ -44,15 +44,29 @@ def GetMemoryType(myLine):
   memoryType = myLine[myLine.find(".") + 1 :myLine.find(" ")]
   return memoryType
 
+def GetFolderName(myLine):
+  levels = myLine.count('/')
+  folderName = " "
+  if (levels >= 2):
+    lastDivider = myLine.rfind("/")
+    otherDivider = myLine.rfind("/",0, lastDivider)
+    folderName = myLine[otherDivider + 1 :lastDivider]
+  return folderName
+
 def GetObjectName(myLine):
   dividerLocation = myLine.rfind("/")
   par = myLine.rfind("(")
-  if (dividerLocation < par):
-    initial = par
+  fill = myLine.count('*fill*')
+  levels = myLine.count('/')
+  folderName = GetFolderName(myLine)
+  if (fill > 0):
+    objectName = "Padding"
+  elif (dividerLocation < par):
+    objectName = myLine[par + 1 :myLine.rfind(".")]
   else:
-    initial = dividerLocation
-  objectName = myLine[initial + 1 :myLine.rfind(".")]
-  return objectName
+    objectName = myLine[dividerLocation + 1 :myLine.rfind(".")]
+
+  return objectName, folderName
 
 def append_list_as_row(fileName, listOfElem):
     with open(fileName, 'a+', newline='') as file:
@@ -61,15 +75,15 @@ def append_list_as_row(fileName, listOfElem):
 
 def main():
   
+  initialTime = datetime.now()
   smallSampleFile = open("MapParser/MapFiles/smallSampleFile.map", "r")
   count = len(open("MapParser/MapFiles/smallSampleFile.map").readlines(  ))
-  initialTime = datetime.now()
 
   if os.path.exists("output.csv"):
     os.remove("output.csv")
 
 
-  rowContent = ["Line", "Memory Type", "Address", "Size" , "Name"]
+  rowContent = ["Line", "Memory Type", "Address", "Size" , "Name", "Folder Name"]
   append_list_as_row('output.csv', rowContent)
 
 
@@ -82,49 +96,13 @@ def main():
         memoryType = GetMemoryType(line)
         sectionAddressStart , sectionTotalSize = GetAddressAndLength(line)
       else:
-        address , size =  GetAddressAndLength(line)
-        if (sectionBreakerFound and IsValidAddress(address, size)):
-          objectName = GetObjectName(line)
-          print("Line: "+ str(x) + " Memory Type " + memoryType + " Address "+ address + " Lenght: " + size + " Object " + objectName )
-          rowContent = [x, memoryType, address, size , objectName]
+        address , sizeHex =  GetAddressAndLength(line)
+        if (sectionBreakerFound and IsValidAddress(address, sizeHex)):
+          objectName , folderName= GetObjectName(line)
+          size = int(sizeHex,0)
+          # print("Line: "+ str(x) + " Memory Type " + memoryType + " Address "+ address + " Lenght: " + str(size) + " Object " + objectName )
+          rowContent = [x, memoryType, address, size , objectName, folderName]
           append_list_as_row('output.csv', rowContent)
-        # else:
-        #   print("Line: "+ str(x)+ "Warning Invalid Address at !!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-
-
-
-
-      # line = smallSampleFile.readline()
-      # initialDot = IsValidLine(line)
-      # if initialDot >= 0:
-      #   if x == 120:
-      #     counterOfLevels = line.count('/')
-
-      #   memoryType, methodDivider = GetMemoryType(initialDot, line)
-      #   method = line[methodDivider : line.find(" ", methodDivider)]
-        
-      #   # method = line[line.find(" ") + len(".text") +1: line.find("0x")]
-      #   countOfHexValues = line.count('0x')
-        
-      #   if countOfHexValues > 1: ## This is the case for when all in same line.
-      #     if countOfHexValues > 2:
-      #       print("Line: "+ str(x)+ "Warning!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-      #     elif countOfHexValues ==2:
-      #       address = line[line.find("0x") :line.find(" ", line.find("0x"))]
-      #       length = line[line.rfind("0x") :line.find(" ", line.rfind("0x"))]
-      #       fileName = line[line.rfind("/") +1 :line.rfind(".o")]
-      #       print("Line: "+ str(x)+ " Memory type: " + memoryType +  " method: "+ method + " Address "+ address + " Lenght: " + length + " File Name: " + fileName)
-              # firstHex = line.find("0x")
-            
-            # print("Line: "+ str(x)+ " Memory type: " + memoryType +  " method: "+ method + " Count of Hex "+ str(countOfHexValues)))
-          # else ## Here is the case for when info is in the next line
-
-
-          # print("Line: "+ str(x)+ " Memory type: " + memoryType +  " method: "+ method + " Count of Hex "+ str(countOfHexValues))
-          # print("Line: "+ str(x)+ " Second Dot Position "+ str(secondDot)+ " method: "+ method +"  Number of lelves is " + str(counterOfLevels) + " in " + line.strip())
-
-
 
   print ("Total Processing time is : " + str(datetime.now() - initialTime)) 
   print ("Total analized lines are  : " + str(count)) 
